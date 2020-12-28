15bb587734c621da7387c711617eb0a998069912
matthewricklefs <ricklefsmatthew1003@gmail.com>
2020-12-28 11:41:17 -0500

    creating handleClick function in Board Component

--- src/components/tic.js
+++ src/components/tic.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={ () => this.setState({value: 'x'}) }>
+        onClick={ () => this.props.onClick() }>
           
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -28,6 +21,12 @@ export class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares: squares })
   }
 
   renderSquare(i) {
